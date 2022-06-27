3d0de97abce9bcd1a8b2fbf7a97a103afb59f97c
Felipe Alves <profelipealves@outlook.com>
2022-06-26 23:14:22 -0300

    implementando função takePhoto

---
+++ script.js
@@ -0,0 +1,34 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUsersMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.srcObject = localMediaStream;
+            video.play();
+        })
+        .catch(err => {
+            console.error(`OH NO!!`, err);
+        });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height);
+
+        pixels = rgbSplit(pixels);
+        ctx.putImageData(pixels, 0, 0);
+    }, 16);
+}
+
+function takePhoto()
